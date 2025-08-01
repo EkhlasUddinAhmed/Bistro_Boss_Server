3bee1013d4d762b9d32d173ad12f3ade2f1b90f6
Ekhlas Uddin Ahmed <matrixedu14@gmail.com>
2025-08-02 05:17:07 +0600

    some type errors are fixed

--- src/app/utils/validateRequest.ts
+++ src/app/utils/validateRequest.ts
@@ -1,8 +1,8 @@
 import { AnyZodObject } from 'zod';
 import catchAsync from './catchAsync';
-
+import { NextFunction, Request, Response } from 'express';
 const validateRequest = (validationSchema: AnyZodObject) => {
-  return catchAsync(async (req, res, next) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     await validationSchema.parseAsync({
       body: req.body,
       cookies: req.cookies,
